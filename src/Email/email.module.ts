4b4d19ec03dbad98dc16d6bf72aae35454926e4d
WillianKakihata <ra-22015763-2@alunos.unicesumar.edu.br>
2025-04-11 05:01:09 -0300

    :sparkles: feat : (SCLM-079) adicionando consummer na comunicação entre o microserviço de autenticacao

--- src/Email/email.module.ts
+++ src/Email/email.module.ts
@@ -2,16 +2,31 @@ import { Module } from '@nestjs/common';
 import { mailController } from './adapters/in/web/controller/email.controller';
 import { SendMailUsecase } from './core/usecases/SendMailUsecase';
 import { EmailMapper } from './adapters/in/web/controller/dto/email.mapper';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { RmqProcessController } from './rabbitmq/rmq-process.controller';
+import { RmqProcessService } from './rabbitmq/rmq-process.service';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule.register([
+        {
+          name: 'EMAIL_IMPORT_SERVICE',
+          transport: Transport.RMQ,
+          options: {
+            urls: ['amqp://localhost:5672'],
+            queue: 'user_import_queue',
+            queueOptions: {
+              durable: false,
+            }
+          }
+        }
+      ])],
   providers: [
-    EmailMapper,
+    EmailMapper, RmqProcessService,
     {
       provide: 'SendEmailInputPort',
       useClass: SendMailUsecase,
     },
   ],
-  controllers: [mailController],
+  controllers: [mailController, RmqProcessController],
 })
 export class MailModule {}
