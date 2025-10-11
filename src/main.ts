3894f9b6277bc717fd04532fbe705c5750c16081
sirafonso <sirafonso005896@outlook.com>
2025-10-11 18:07:18 -0300

    :bug: fix: fix deploy

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import type { MicroserviceOptions } from '@nestjs/microservices';
 import { Transport } from '@nestjs/microservices';
 import { HttpExceptionFilter } from './Email/core/domain/exceptions/http.exception.filter';
 import type { INestApplication } from '@nestjs/common';
+import { CustomConfigService } from './Common/services/custom.config.service';
 
-async function setupMicroservices(app: INestApplication): Promise<void> {
+async function setupMicroservices(
+  app: INestApplication,
+  cs: CustomConfigService,
+): Promise<void> {
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://localhost:5672'],
+      urls: [cs.get<string>('RABBITMQ_URL')],
       queue: 'validate_user_email_queue',
       queueOptions: {
         durable: false,
@@ -26,9 +30,10 @@ function setGlobalMiddlewares(app: INestApplication): void {
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  const cs = app.get<CustomConfigService>(CustomConfigService);
 
   setGlobalMiddlewares(app);
-  await setupMicroservices(app);
+  await setupMicroservices(app, cs);
 }
 
 void bootstrap();
